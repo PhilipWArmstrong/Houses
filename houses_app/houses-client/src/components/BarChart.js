823c56ff6e8d2367523e562b4b97e89c2172cda3
Philip Armstrong <philiparmstrong@Philips-MacBook-Pro.local>
2020-02-16 20:07:22 -0600

    error Line 49:15:  'listedsCharts' is assigned a value but never used

---
+++ houses_app/houses-client/src/components/BarChart.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Chart from 'chart.js'
+
+class BarChart extends Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {}
+        this.createChart = this.createChart.bind(this)
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData = () => {
+        fetch('http://localhost:3000/houses/1')                                        
+            .then(response => response.json())                                            
+            .then(json => this.prepareData(json))
+            .then(data => this.createChart(data));
+    }
+
+    prepareData = (data) => {
+        const chartData = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Sold Price',
+                    data: [],
+                    lineTension: 0
+                },
+                {
+                    label: 'Listed Price',
+                    data: []
+                }
+            ]
+        }
+        data.listeds.forEach(listed => {
+            chartData.labels.push(listed.id)
+            chartData.datasets[0].data.push(listed.sold_price)
+            chartData.datasets[1].data.push(listed.listed_price)
+        })
+        console.log(chartData)
+        return chartData
+    }
+
+    createChart (data) {
+        const ctx = document.querySelector('#houses')
+        const listedsCharts = new Chart(ctx, {
+            type: 'line',
+            data: data
+        })
+    }
+
+    render () {
+        return (
+        <>
+            <h1>History of Home Sales</h1>
+            <canvas id="houses" width="300" height="100"></canvas>
+        </>
+        )
+    }
+}
+
+export default BarChart;
